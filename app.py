998a570138ed211f936cc0b2239b05b45dcd29c1
andreabassetti <andreajr@bassetti.net>
2021-12-14 16:05:34 -0800

    commit to main

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
